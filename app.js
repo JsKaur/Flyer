70eb7aae6801442a98bca5823792aa46edb138b7
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-17 10:48:09 -0500

    making flyer

---
+++ app.js
@@ -0,0 +1,12 @@
+// button=document.getElementById("button");
+// button.addEventListener("click",myFunction);
+
+// function myFunction(){
+    
+// }
+
+function redirectToForm() {
+    window.location.href = "form.html";
+}
+
+
